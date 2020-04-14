67603ee0dd50e20f5b3e369e091ea51887403fcc
jtobelem-simplon <jtobelem@simplon.co>
2020-04-14 16:01:28 +0200

    améliore le retour exception controller

--- src/main/java/hibernate/mtm/controller/AuteurController.java
+++ src/main/java/hibernate/mtm/controller/AuteurController.java
@@ -2,6 +2,7 @@ package hibernate.mtm.controller;
 
 import hibernate.mtm.model.Auteur;
 import hibernate.mtm.repository.AuteurRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +35,7 @@ public class AuteurController {
 
 //    Gestion des exceptions :
 //    https://dzone.com/articles/exception-handling-spring-rest
+
     @GetMapping(path = "/{auteurID}")
     public @ResponseBody
     Auteur getOne(@PathVariable long auteurID) throws AuteurNotFoundException {
@@ -43,5 +45,11 @@ public class AuteurController {
         else {
             throw new AuteurNotFoundException();
         }
+    }
+
+    @PostMapping
+    @ResponseStatus(value= HttpStatus.CREATED, reason = "cet auteur not found")
+    public Auteur create() {
+        return null;
     }
 }
--- src/main/java/hibernate/mtm/controller/AuteurNotFoundException.java
+++ src/main/java/hibernate/mtm/controller/AuteurNotFoundException.java
@@ -3,6 +3,6 @@ package hibernate.mtm.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.NOT_FOUND)
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason = "cet auteur not found")
 public class AuteurNotFoundException extends Exception {
 }
